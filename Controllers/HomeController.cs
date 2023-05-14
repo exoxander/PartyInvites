26bb8fc546097c1667c7a3320622d737ae442162
alexander l <exoxander15@gmail.com>
2023-05-14 10:39:27 -0600

    completed tutorial project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,9 +16,29 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponseModel response)
+        {
+            if (ModelState.IsValid)
+            {
+                RepositoryModel.AddResponse(response);
+                return View("Thanks", response);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(RepositoryModel.Responses.Where(x => x.WillAttend == true));
         }
 
     }
---
+++ Models/RepositoryModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PartyInvites.Models
+{
+    public class RepositoryModel
+    {
+        public static List<GuestResponseModel> responses = new();
+        public static IEnumerable<GuestResponseModel> Responses => responses;
+        public static void AddResponse(GuestResponseModel response)
+        {
+            Console.WriteLine(response);
+            responses.Add(response);
+        }
+    }
+}
